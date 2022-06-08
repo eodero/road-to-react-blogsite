96e61a320838bdc550a4061f1856ea8ba5cd412c
eodero <erickarthurs13@gmail.com>
2022-06-07 21:50:29 -0500

    used JSX to embed JS in the App  component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
 
+//--can use JSX to refer to variables or ven functions using curly braces({}) in the html body----//
+
+// const title = 'React';
+function getTitle(title) {
+  return title;
+}
 function App() {
   return (
     // the following block of code is JSX
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getTitle('React')} </h1>
+      {/* in jsx attributes have different names from html e.g htmlFor */}
+      <label htmlFor='search'>Search: </label>
+      <input id="search" type="text" />
     </div>
   )
 
